a48bb0e2b12ca9223a279d5e93981d4768ad1b3b
Jual Rana <contactjual@gmail.com>
2024-12-16 19:23:47 +0600

    done apps but need more social link

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-alert('Updating')
+let myLinks = ['https://www.facebook.com/', 'https://www.youtube.com/', 'https://www.instagram.com/', 'https://www.tiktok.com/', 'https://www.x.com/', 'https://www.linkedin.com/']
+
+function manageLinks() {
+    const myRandomNumber = Math.floor(Math.random() * ((myLinks.length -1) - 0 + 1) - 0);
+    // console.log(myRandomNumber);
+    document.getElementById('butto').href = myLinks[myRandomNumber];
+}
+
+setInterval(manageLinks, 500);
